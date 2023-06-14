d0800b96d871d7364d569d73ef8a31accfb2f88d
Moshe <mosheyaku@gmail.com>
2023-06-14 19:49:34 +0300

    Adding Monitor object to class and use it.

--- src/main/java/com/example/trafficlight/TrafficControl.java
+++ src/main/java/com/example/trafficlight/TrafficControl.java
@@ -6,15 +6,18 @@ public class TrafficControl extends Thread {
     private TrafficLight lowerLight;
     private TrafficLight rightLight;
     private TrafficLight leftLight;
+    private Monitor monitor;
 
-    public TrafficControl(TrafficLight upperLight, TrafficLight lowerLight, TrafficLight rightLight, TrafficLight leftLight) {
+    public TrafficControl(TrafficLight upperLight, TrafficLight lowerLight, TrafficLight rightLight, TrafficLight leftLight, Monitor monitor) {
         this.upperLight = upperLight;
         this.lowerLight = lowerLight;
         this.rightLight = rightLight;
         this.leftLight = leftLight;
+        this.monitor = monitor;
     }
 
     public void run() {
         super.run();
+        monitor.displayLights(upperLight, lowerLight, rightLight, leftLight);
     }
 }
