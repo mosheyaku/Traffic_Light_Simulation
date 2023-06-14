594746dbe44f9d5e42268d317af9e73d7d7d9c55
Moshe <mosheyaku@gmail.com>
2023-06-14 17:57:42 +0300

    Adding TrafficControl class constructor and run method.

--- src/main/java/com/example/trafficlight/TrafficControl.java
+++ src/main/java/com/example/trafficlight/TrafficControl.java
@@ -1,8 +1,20 @@
 package com.example.trafficlight;
 
-public class TrafficControl extends  Thread {
+public class TrafficControl extends Thread {
 
-    public void run(){
+    private TrafficLight upperLight;
+    private TrafficLight lowerLight;
+    private TrafficLight rightLight;
+    private TrafficLight leftLight;
+
+    public TrafficControl(TrafficLight upperLight, TrafficLight lowerLight, TrafficLight rightLight, TrafficLight leftLight) {
+        this.upperLight = upperLight;
+        this.lowerLight = lowerLight;
+        this.rightLight = rightLight;
+        this.leftLight = leftLight;
+    }
+
+    public void run() {
         super.run();
     }
 }
